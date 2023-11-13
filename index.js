559380425655d068c0053ed6ea3dce39b791fff4
zhangxt <1535852024@qq.com>
2023-11-13 10:00:23 +0800

    取消提交

---
+++ index.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require("electron");
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 850,
+    title: "CAN曲线分析工具(CSV)",
+  });
+  win.removeMenu();
+  win.loadFile("index.html");
+};
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
